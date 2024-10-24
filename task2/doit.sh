a22cc74e8b284127adcd8cb0927ddb862333a9f4
S-mishra07 <sm5423@ic.ac.uk>
2024-10-24 14:22:28 +0100

    Initial Commit

---
+++ task2/doit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+~/Documents/iac/lab0-devtools/tools/attach_usb.sh
+#cleanup
+rm -rf obj_dir
+rm -f sinegen.vcd
+
+#run Verilator to translate Verilog to C++, including C++ testbench
+verilator -Wall --cc --trace sinegen.sv --exe sinegen_tb.cpp
+
+#build C++ project
+make -j -C obj_dir/ -f Vsinegen.mk Vsinegen
+
+#run executable simulation file
+obj_dir/Vsinegen
+
---
+++ task2/sinegen.sv
@@ -0,0 +1,43 @@
+module sinegen #(
+    parameter   A_WIDTH = 8,
+                D_WIDTH = 8
+)(
+    //interface signals
+    input logic                 clk,
+    input logic                 rst,
+    input logic                 en,
+    input logic [D_WIDTH-1:0]   incr,           //increment for addr counter
+    input logic [D_WIDTH-1:0]   phase_offset,   //phase_offset input
+    output logic [D_WIDTH-1:0]  dout1,            //output data 
+    output logic [D_WIDTH-1:0]  dout2            //output data
+);
+
+logic   [A_WIDTH-1:0]           address1;        //interconnecting the wire
+logic   [A_WIDTH-1:0]           address2;        //interconnecting the wire
+
+counter addrCounter(
+    .clk (clk),
+    .rst (rst),
+    .en (en),
+    .incr (incr),
+    .count (address1)
+);
+
+    assign address2 = address1 + phase_offset;
+
+rom sineRom (
+    .clk (clk),
+    .addr1 (address1),
+    .addr2 (address2),
+    .dout1 (dout1),
+    .dout2 (dout2),
+    .phase_offset (phase_offset)
+);
+
+endmodule
+
+
+
+
+
+
